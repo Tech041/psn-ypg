80cd5a9e312c70d70b2df933f34e0147a064ecf5
nelson <chinwubanelson@gmail.com>
2026-04-18 19:55:48 +0100

    removed unused service object

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const services = [
-  "E-commerce applications",
-  "Business landing pages",
-  "portfolio websites",
-  "real estate applications",
-  "dashboards",
-  "job portals",
-  "learning management systems",
-];
-
 const About = () => {
   return (
     <section className="w-full min-h-screen flex items-center bg-gradient-to-b from-green-50 to-white pt-20 pb-10">
